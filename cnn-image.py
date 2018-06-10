839e17ceb64c22add348f02b92e1ed123cd6bd25
= <=>
2018-06-09 23:07:02 -0300

    data declared, converted and normalized

--- cnn-image.py
+++ cnn-image.py
@@ -1,32 +1,32 @@
-#from keras.layers.core import Dense, Dropout, Activation, Flatten
-#from keras.layers.convolutional import Convolution2D, MaxPooling2D
-#from keras.optimizers import SGD, RMSprop, adam
-#from keras.utils import np_utils
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD, RMSprop, adam
+from keras.utils import np_utils
+
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import os
-#import theano
+import theano
 from PIL import Image
 from numpy import *
-from random import randrange
 
-#from sklearn.utils import shufflem #You can use this instead of shuffleData
-#from sklearn.cross_validation import train_test_split #You can use this instead of splitData
 
-def prepareDataset(input_path, output_path, output_image_height, output_image_width,
+def prepareData(input_path, output_path, output_image_height, output_image_width,
                    labels_ammount_array):
     input_images = getImages(input_path)
     for file in input_images:
-        image = input_path + '/' + file
+        image = input_path + '\\' + file
         resized_image    = resizeImage(output_image_height, output_image_width, image)
         grayscaled_image = convertToGrayscale(resized_image)
         grayscaled_image.save(output_path + '\\' + file, 'JPEG')
     images_as_array = imagesToArray(output_path)  
     labels = createLabels(labels_ammount_array)
-    train_data = shuffleData(images_as_array, labels)
-    return train_data
+    data, labels = shuffle(images_as_array, labels, random_state=2)
+    return data, labels
         
 def resizeImage(height, width, image):
     input_image  = Image.open(image)
@@ -56,31 +56,27 @@ def createLabels(labels_amount_array):
         previous_label_begin = next_label_begin
     return labels
 
-def shuffleData(data, labels):
-    permutation_order = np.random.permutation(len(data))
-    shuffled_data, shuffled_labels = data[permutation_order], labels[permutation_order]
-    shuffled_data_labels  = [shuffled_data, shuffled_labels]
-    return shuffled_data_labels
-
-def splitData(dataset, split):    
-    train_size = split * len(dataset)
-    data   = dataset[0]
-    labels = dataset[1]
-    train_data   = list()
-    train_labels = list()
-    data_copy    = list(data)
-    labels_copy  = list(labels)
-    while len(train_data) < train_size:
-        index = randrange(len(data_copy))
-        train_data.append(data_copy.pop(index))
-        train_labels.append(labels_copy.pop(index))
-    return train_data, data_copy, train_labels, labels_copy
-
 #Add parameter cmap = 'gray' in imshow for grayscale
 def previewImage(images_as_array):
     img = images_as_array.reshape(image_height, image_width)
     plt.imshow(img)
     plt.show()
+
+def declareDimensionDepth(X_train, X_test):
+    X_train = X_train.reshape(X_train.shape[0], image_depth, image_height, image_width)
+    X_test  = X_test.reshape(X_test.shape[0], image_depth, image_height, image_width)
+    return X_train, X_test
+
+def convertDataType(X_train, X_test):
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    return X_train, X_test
+
+def normalizeValues(X_train, X_test, value):
+    X_train /= value
+    X_test /= value
+    return X_train, X_test
+    
 
 #Config
 input_path  = './input_data'
@@ -88,17 +84,29 @@ output_path = './output_data'
 
 image_height = 100
 image_width  = 100
+labels_amount = [1,2,1]
 
 batch_size = 50
 number_of_classes = 3
 number_of_epoch   = 20
 
-image_channels    = 1
+image_depth       = 1
 number_of_filters = 32 #?
 number_if_pool    = 2
 number_of_convolution = 3
 
-    
-dataset = prepareDataset(input_path, output_path, image_height, image_width,[1,3,1])
+#Data set up
+data, labels = prepareData(input_path, output_path, image_height, image_width, labels_amount)
 
-X_train, X_test, y_train, y_test = splitData(dataset,0.5)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=4)
+
+X_train, X_test = declareDimensionDepth(X_train, X_test)
+X_train, X_test = convertDataType(X_train, X_test)
+X_train, X_test = normalizeValues(X_train,X_test,255)
+
+Y_train = np_utils.to_categorical(y_train, len(labels_amount))
+Y_test  = np_utils.to_categorical(y_test, len(labels_amount))
+
+#Model	
+model  = Sequential()
+
