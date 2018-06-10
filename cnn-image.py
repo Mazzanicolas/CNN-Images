5b4f20b8ccc539998d563c09310d0e8fd53fa397
= <=>
2018-06-09 21:57:23 -0300

    implemented shuffle and split

--- cnn-image.py
+++ cnn-image.py
@@ -10,13 +10,10 @@ import os
 #import theano
 from PIL import Image
 from numpy import *
+from random import randrange
 
-#from sklearn.utils import shuffle
-#from sklearn.cross_validation import train_test_split
-
-#Full path to avoid permission denied
-input_path  = 'C:/Users/cclar_000/Desktop/AI/CNN-MNIST-mod/input_data'
-output_path = './output_data'
+#from sklearn.utils import shufflem #You can use this instead of shuffleData
+#from sklearn.cross_validation import train_test_split #You can use this instead of splitData
 
 def prepareDataset(input_path, output_path, output_image_height, output_image_width,
                    labels_ammount_array):
@@ -26,14 +23,11 @@ def prepareDataset(input_path, output_path, output_image_height, output_image_wi
         resized_image    = resizeImage(output_image_height, output_image_width, image)
         grayscaled_image = convertToGrayscale(resized_image)
         grayscaled_image.save(output_path + '\\' + file, 'JPEG')
-    images_as_array = imagesToArray(output_path)
+    images_as_array = imagesToArray(output_path)  
     labels = createLabels(labels_ammount_array)
-    permutation_order = np.random.permutation(len(images_as_array))
-    data, label = images_as_array[permutation_order], labels[permutation_order]
-    train_data  = [data, label]
+    train_data = shuffleData(images_as_array, labels)
     return train_data
         
-
 def resizeImage(height, width, image):
     input_image  = Image.open(image)
     return input_image.resize((height, width))
@@ -62,5 +56,49 @@ def createLabels(labels_amount_array):
         previous_label_begin = next_label_begin
     return labels
 
-train_data = prepareDataset(input_path, output_path, 100, 100,[1,3,1])
+def shuffleData(data, labels):
+    permutation_order = np.random.permutation(len(data))
+    shuffled_data, shuffled_labels = data[permutation_order], labels[permutation_order]
+    shuffled_data_labels  = [shuffled_data, shuffled_labels]
+    return shuffled_data_labels
 
+def splitData(dataset, split):    
+    train_size = split * len(dataset)
+    data   = dataset[0]
+    labels = dataset[1]
+    train_data   = list()
+    train_labels = list()
+    data_copy    = list(data)
+    labels_copy  = list(labels)
+    while len(train_data) < train_size:
+        index = randrange(len(data_copy))
+        train_data.append(data_copy.pop(index))
+        train_labels.append(labels_copy.pop(index))
+    return train_data, data_copy, train_labels, labels_copy
+
+#Add parameter cmap = 'gray' in imshow for grayscale
+def previewImage(images_as_array):
+    img = images_as_array.reshape(image_height, image_width)
+    plt.imshow(img)
+    plt.show()
+
+#Config
+input_path  = './input_data'
+output_path = './output_data'
+
+image_height = 100
+image_width  = 100
+
+batch_size = 50
+number_of_classes = 3
+number_of_epoch   = 20
+
+image_channels    = 1
+number_of_filters = 32 #?
+number_if_pool    = 2
+number_of_convolution = 3
+
+    
+dataset = prepareDataset(input_path, output_path, image_height, image_width,[1,3,1])
+
+X_train, X_test, y_train, y_test = splitData(dataset,0.5)
